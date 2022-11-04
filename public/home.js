b422bf939b7e35f416f9cab0bd22a5387ccc3c8a
Jared Clyde <jarclyde@gmail.com>
2022-11-04 09:33:31 -0600

    molayo

--- public/home.js
+++ public/home.js
@@ -4,7 +4,5 @@ const hideMe = document.querySelector('#hideMe')
 button.addEventListener('click', () => {
 	console.log('button pushed')
 	hideMe.classList.toggle('hide')
-	axios.get('/fail').then(res => {console.log(res)})
+	axios.get('/crit').then(res => {console.log(res)})
 })
-
-// The resource from “https://deployment7882.herokuapp.com/js” was blocked due to MIME type (“text/html”) mismatch (X-Content-Type-Options: nosniff).
